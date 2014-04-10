6a37deaedfd1d440c7b56bcba2095b108a12429f
Joshua Rubin <jrubin@zvelo.com>
2014-04-10 03:39:40 -0600

    clean up
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- commands/marantz.go
+++ commands/marantz.go
@@ -8,8 +8,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	defaultServerHost = "localhost"
+	defaultServerPort = uint(6932)
+)
+
 var (
 	verbose bool // TODO(jrubin) use this flag
+
+	serverCfg struct {
+		host string
+		port uint
+	}
 
 	marantzCmd = &cobra.Command{
 		Use:   "marantz",
@@ -33,7 +43,26 @@ func init() {
 
 	viper.SetConfigName("config")
 
+	viper.SetDefault("server", map[string]interface{}{
+		"host": defaultServerHost,
+		"port": defaultServerPort,
+	})
+
 	marantzCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	marantzCmd.PersistentFlags().StringVarP(&serverCfg.host, "host", "h", defaultServerHost, "server host name (remote for client commands, listen for server)")
+	marantzCmd.PersistentFlags().UintVarP(&serverCfg.port, "port", "p", defaultServerPort, "server port")
+}
+
+func initServerConfig() {
+	s := viper.GetStringMap("server")
+
+	if !marantzCmd.PersistentFlags().Lookup("host").Changed {
+		serverCfg.host = s["host"].(string)
+	}
+
+	if !marantzCmd.PersistentFlags().Lookup("port").Changed {
+		serverCfg.port = s["port"].(uint)
+	}
 }
 
 func Execute() error {
--- commands/server.go
+++ commands/server.go
@@ -11,24 +11,12 @@ import (
 )
 
 const (
-	defaultListenAddress = "0.0.0.0"
-	defaultListenPort    = uint(6932)
-	defaultSerialPort    = "/dev/ttyUSB0"
-	defaultBaudRate      = 9600
+	defaultSerialPort = "/dev/ttyUSB0"
+	defaultBaudRate   = 9600
 )
 
 var (
-	serverCfg struct {
-		listen struct {
-			address string
-			port    uint
-		}
-
-		serial struct {
-			port     string
-			baudRate int
-		}
-	}
+	serialCfg serial.Config
 
 	serverCmd = &cobra.Command{
 		Use:   "server",
@@ -47,57 +35,35 @@ var (
 )
 
 func init() {
-	viper.SetDefault("server", map[string]interface{}{
-		"listen": map[string]interface{}{
-			"address": defaultListenAddress,
-			"port":    defaultListenPort,
-		},
-		"serial": map[string]interface{}{
-			"port": defaultSerialPort,
-			"baud": defaultBaudRate,
-		},
+	serialCh.read = make(chan []byte, 128)
+	serialCh.write = make(chan []byte, 128)
+	serialCh.err = make(chan error, 128)
+
+	viper.SetDefault("serial", map[string]interface{}{
+		"port": defaultSerialPort,
+		"baud": defaultBaudRate,
 	})
 
-	serverCmd.Flags().StringVarP(&serverCfg.listen.address, "address", "a", defaultListenAddress, "listen address")
-	serverCmd.Flags().UintVarP(&serverCfg.listen.port, "port", "p", defaultListenPort, "listen port")
-	serverCmd.Flags().StringVarP(&serverCfg.serial.port, "serial", "s", defaultSerialPort, "serial port")
-	serverCmd.Flags().IntVarP(&serverCfg.serial.baudRate, "baud", "b", defaultBaudRate, "serial port baud rate")
+	serverCmd.Flags().StringVarP(&serialCfg.Name, "serial", "s", defaultSerialPort, "serial port")
+	serverCmd.Flags().IntVarP(&serialCfg.Baud, "baud", "b", defaultBaudRate, "serial port baud rate")
 
 	serverCmdP = serverCmd
 
 	marantzCmd.AddCommand(serverCmd)
 }
 
-func initConfig() {
-	server := viper.GetStringMap("server")
+func initSerialConfig() {
+	initServerConfig()
 
-	l := server["listen"].(map[string]interface{})
-	s := server["serial"].(map[string]interface{})
-
-	if !serverCmdP.Flags().Lookup("address").Changed {
-		serverCfg.listen.address = l["address"].(string)
-	}
-
-	if !serverCmdP.Flags().Lookup("port").Changed {
-		serverCfg.listen.port = l["port"].(uint)
-	}
+	s := viper.GetStringMap("serial")
 
 	if !serverCmdP.Flags().Lookup("serial").Changed {
-		serverCfg.serial.port = s["port"].(string)
+		serialCfg.Name = s["port"].(string)
 	}
 
 	if !serverCmdP.Flags().Lookup("baud").Changed {
-		serverCfg.serial.baudRate = s["baud"].(int)
+		serialCfg.Baud = s["baud"].(int)
 	}
-}
-
-func openSerial() (io.ReadWriteCloser, error) {
-	serialCh.read = make(chan []byte, 128)
-	serialCh.write = make(chan []byte, 128)
-	serialCh.err = make(chan error, 128)
-
-	c := &serial.Config{Name: serverCfg.serial.port, Baud: serverCfg.serial.baudRate}
-	return serial.OpenPort(c)
 }
 
 func serialWatcher(s io.ReadWriteCloser) {
@@ -121,9 +87,9 @@ func serialWatcher(s io.ReadWriteCloser) {
 }
 
 func server(cmd *cobra.Command, args []string) {
-	initConfig()
+	initSerialConfig()
 
-	s, err := openSerial()
+	s, err := serial.OpenPort(&serialCfg)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
